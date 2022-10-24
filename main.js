23a21178edef01ea1a83704f3951b2a97c571f55
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-10-24 16:28:36 +0200

    Secondo tentativo

--- main.js
+++ main.js
@@ -3,23 +3,33 @@ let mail = ["pippo@gmail.com", "pinco@gmail.com", "pallino@gmail.com"];
 
 const inputMail = document.getElementById("exampleInputEmail1");
 
+let soldato= 0;
 
 function iscriviti() {
+
     let valueMail = inputMail.value;
     // console.log(valueMail);
 
     for (let i=0; i < mail.length; i++) {
-        // console.log(mail[i]);
+        console.log(mail[i]);
 
-        if(mail[i] == valueMail){
-            // console.log("Successo");
-            document.getElementById("rispostaSistema").innerHTML= `<div class="iscritto">Risulti essere iscritto</div>`;
+        if(mail[i] === valueMail){
+            let soldato= 1;
+    
         }
-        else {
-            document.getElementById("rispostaSistema").innerHTML= `<div class="nonIscritto">Non sei iscritto</div>`;
-        }
+       
     }
+
+    if (soldato= 1){
+        document.getElementById("rispostaSistema").innerHTML= `<div class="iscritto">Risulti essere iscritto</div>`;
+    }
+    else{
+        document.getElementById("rispostaSistema").innerHTML= `<div class="nonIscritto">Non sei iscritto</div>`;
+    }
+
+
 }
+
 
 
 
