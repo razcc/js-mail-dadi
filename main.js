0c23caeafe17903543abed5dd7aa12ef6274aa96
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-10-24 15:52:54 +0200

    Primo tentativo di ciclo for

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let mail = ["pippo@gmail.com", "pinco@gmail.com"];
+let mail = ["pippo@gmail.com", "pinco@gmail.com", "pallino@gmail.com"];
 
 
 const inputMail = document.getElementById("exampleInputEmail1");
@@ -6,7 +6,18 @@ const inputMail = document.getElementById("exampleInputEmail1");
 
 function iscriviti() {
     let valueMail = inputMail.value;
-    console.log(valueMail);
+    // console.log(valueMail);
+
+    for (let i=0; i < mail.length; i++) {
+        // console.log(mail[i]);
+
+        if(mail[i] == valueMail){
+            console.log("Successo");
+        }
+        else{
+            console.log("Fail")
+        }
+    }
 }
 
 
